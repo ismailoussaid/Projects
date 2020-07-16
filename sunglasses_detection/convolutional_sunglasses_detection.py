5a4310d512ed00a252d3862f291bae633ace3f6c
ismailoussaid <59666904+ismailoussaid@users.noreply.github.com>
2020-07-16 11:53:13 +0200

    added data augmentation function, architecture function

--- sunglasses_detection/convolutional_sunglasses_detection.py
+++ sunglasses_detection/convolutional_sunglasses_detection.py
@@ -11,7 +11,7 @@ import timeit
 from tensorflow.core.framework import graph_pb2
 from tensorflow.python.framework import importer
 
-epochs = 10
+epochs = 15
 images_paths = "C:/Users/Ismail/Documents/QuividiData/cropped_images"
 global_path = "C:/Users/Ismail/Documents/Projects"
 TEST_PROPORTION = 0.05
@@ -21,6 +21,13 @@ def open_resize(path, liste):
     #resize an image loaded from path and adds it to a list such that it is ready for tensorflow (shape=(x,y,channel))
     img = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)/255
     img = cv2.resize(img, (shape, shape)).reshape((shape, shape, channel))
+    liste.append(img)
+
+def open_flip_resize(path, liste):
+    #resize an image loaded from path and adds it to a list such that it is ready for tensorflow (shape=(x,y,channel))
+    img = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY)/255
+    img = cv2.resize(img, (shape, shape)).reshape((shape, shape, channel))
+    img = cv2.flip(img, 1)
     liste.append(img)
 
 def build_folder(path, name):
@@ -35,7 +42,30 @@ def globalize(path):
     #make path in relation with an existing directory
     return global_path + path
 
-def build_dataset(majority_amount):
+def build_dataset(majority_amount, augmentation):
+#majority_amount : maximum number of sunglasses examples in dataset
+#augmentation boolean that does data augmentation with miroring effect on minority images if True
+    compteur = 0
+    image_set, labels = [], []
+
+    for path in glob.glob(images_paths + '/*.jpg'):
+        if 'no' in path:
+            if compteur < majority_amount:
+                #ensure that we have less than the authorized amount of majority class samples
+                open_resize(path, image_set)
+                labels.append(0)
+                compteur += 1
+            else: continue
+        else:
+            open_resize(path, image_set)
+            labels.append(1)
+            if augmentation == True:
+                open_flip_resize(path, image_set)
+                labels.append(1)
+
+    return image_set, labels
+
+def build_augmented_dataset(majority_amount):
 #majority_amount : maximum number of sunglasses examples in dataset
     compteur = 0
     image_set, labels = [], []
@@ -82,7 +112,7 @@ def labelize(outputs):
         index_min = np.argmin(output)
         output[index_min] = 0
 
-images, classes = build_dataset(3000)
+images, classes = build_dataset(3000, False)
 print("<== Dataset is loaded  ==>")
 
 #transform classes list to a binary matrix representation of the input so tensorflow can work with it
@@ -93,7 +123,7 @@ images, images_test, classes, classes_test = separate_test_train(images, classes
 def plot_epoch(history, metric, filename):
     #takes history of model.fit and extract training and validation metric data
     #save the curve in filename
-    horizontal_axis = np.array([epoch for epoch in range(1, epochs + 1)])
+    horizontal_axis = np.array([epoch for epoch in range(1, len(history.history[metric])+1)])
     # Plot the metric curves for training and validation
     training = np.array(history.history[metric])
     validation = np.array(history.history['val_{}'.format(metric)])
@@ -109,29 +139,12 @@ def multiple_append(listes, elements):
     #append different elements to different lists in the same time
     if len(listes) != len(elements):
         #ensure, there is no mistake in the arguments
-        print("listes and elements do not have the same length")
+        print("lists and elements do not have the same length")
     else:
         for k in range(len(listes)):
             liste = listes[k]
             element = elements[k]
             liste.append(element)
-
-def get_flops(model_h5_path):
-    session = tf.compat.v1.Session()
-    graph = tf.compat.v1.get_default_graph()
-
-    with graph.as_default():
-        with session.as_default():
-            model = tf.keras.models.load_model(model_h5_path)
-
-            run_meta = tf.compat.v1.RunMetadata()
-            opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
-
-            # We use the Keras session graph in the call to the profiler.
-            flops = tf.compat.v1.profiler.profile(graph=graph,
-                                                  run_meta=run_meta, cmd='op', options=opts)
-
-            return flops.total_float_ops
 
 def architecture(nb_dense, param_dense, nb_conv, param_conv):
     if nb_conv>len(param_conv):
@@ -140,7 +153,7 @@ def architecture(nb_dense, param_dense, nb_conv, param_conv):
         print("too much conv parameters")
     if nb_dense>len(param_dense):
         print("not enough dense parameters")
-    else:
+    elif nb_dense<len(param_dense):
         print("too much dense parameters")
 
     if nb_conv==len(param_conv) and nb_dense==len(param_dense):
@@ -163,17 +176,17 @@ def architecture(nb_dense, param_dense, nb_conv, param_conv):
 batches = [256, 128, 64, 32, 16, 8, 4]
 fs = [256, 128, 64, 32]
 ds = [128, 64, 32, 16, 8, 4]
-layers_number = 2_1
-conv_nb, dense_nb = 2, 1
+layers_number = 1_1
+conv_nb, dense_nb = 1, 1
 
 def build_model():
     #create folder to save all tha callbacks
     #build columns for the final resume
     #test all the combinaisons of batch size, conv2d's num_filter, dense's number of nodes
 
-    path_model = globalize("/model_{}_layers/models").format(layers_number)
-    path_logger = globalize("/model_{}_layers/csv_logger").format(layers_number)
-    path_curves = globalize("/model_{}_layers/curves").format(layers_number)
+    path_model = globalize("/model_{}_layers_size_{}_{}/models").format(layers_number, shape, shape)
+    path_logger = globalize("/model_{}_layers_size_{}_{}/csv_logger").format(layers_number, shape, shape)
+    path_curves = globalize("/model_{}_layers_size_{}_{}/curves").format(layers_number, shape, shape)
 
     paths = [path_logger, path_curves, path_model]
     names = ['logger', 'loss curves', 'model']
@@ -187,14 +200,14 @@ def build_model():
     true = classes_test
     score = f1_score(true, majority_estimation, average='micro')
     print("f1-score for majority estimator : {}".format(score))
-    scores, col_f, col_d, col_batch, conv, dense, execution_time, flops = [score, 1-score], [0, 0], [0, 0], [0, 0], [0, 0],\
+    scores, col_f, col_d, col_batch, conv, dense, execution_time = [score, 1-score], [0, 0], [0, 0], [0, 0], [0, 0],\
                                                                           ['majority estimator','minority_estimator'], \
-                                                                          [0, 0], [0, 0]
+                                                                          [0, 0]
 
     for batch in batches:
         for f in fs:
             for d in ds:
-                model = architecture(dense_nb, [d], conv_nb, [f, f//2])
+                model = architecture(dense_nb, [d], conv_nb, [f])
 
                 # Compile the model
                 model.compile(optimizer = 'adam', loss = "binary_crossentropy",
@@ -203,19 +216,22 @@ def build_model():
                 print("Model compiled")
 
                 # Callbacks
-                checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=path_model+"/model_f_{}_d_{}_batch_{}.hdf5".format(f, d, batch),
+                model_file_path = path_model+"/model_f_{}_d_{}_batch_{}.h5".format(f, d, batch)
+                checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=model_file_path,
                                                                save_best_only=True, period=epochs,
                                                                monitor='val_loss', mode='max')
 
                 reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2,
-                                                               node='max', patience=3, min_lr=1e-06)
+                                                               node='max', patience=1, min_lr=1e-12)
 
                 csv_logger = tf.keras.callbacks.CSVLogger(path_logger+"/logger_model_f_{}_d_{}_batch_{}.log".format(f, d, batch),
                                                           separator=",", append=False)
 
+                earlystopping = tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss')
+
                 history_conv = model.fit(images, classes, batch_size = batch, epochs = epochs,
                                           validation_split=0.1, verbose = 2, shuffle = True,
-                                         callbacks = [checkpoint, reduce_lr, csv_logger])
+                                         callbacks = [checkpoint, reduce_lr, csv_logger, earlystopping])
                 print("Model trained")
 
                 metrics = ['recall', 'precision']
@@ -232,21 +248,9 @@ def build_model():
 
                 plt.close('all')
 
-                #compute flops of the  model
-                flop = get_flops(path_model + "/model_f_{}_d_{}_batch_{}.hdf5".format(f, d, batch))
-
-                # load best model (corresponding to the model for best epoch)
-                model = tf.keras.models.load_model(path_model + "/model_f_{}_d_{}_batch_{}.hdf5".format(f, d, batch))
-                model.compile(optimizer='adam', loss="binary_crossentropy",
-                              metrics=[tf.keras.metrics.Recall(name='recall'),
-                                       tf.keras.metrics.Precision(name='precision')])
-                print("Model loaded")
-
                 start = timeit.default_timer()
                 predictions = model.predict(images_test)
                 stop = timeit.default_timer()
-
-                flop = get_flops(path_model + "/model_f_{}_d_{}_batch_{}.hdf5".format(f, d, batch))
 
                 labelize(predictions) #gives 1 to the most likely label and 0 otherwise
                 print("Prediction done")
@@ -255,18 +259,23 @@ def build_model():
                 score = f1_score(true, predictions, average='micro') #compute f1-score
                 print("Score computed")
 
-                multiple_append([col_f, col_d, col_batch, scores, conv, dense, execution_time, flops],
-                                [f, d, batch, score, conv_nb, dense_nb, time, flop])
+                multiple_append([col_f, col_d, col_batch, scores, conv, dense, execution_time],
+                                [f, d, batch, score, conv_nb, dense_nb, time])
                 print("Columns updated")
 
-    print("Building Dataframe")
+                #save dataframe each time and we update it
+                print("Building Dataframe")
 
-    d = {'number of Conv2D' : conv, 'number of Dense' : dense, 'f': col_f, 'd': col_d,
-         'batch size': col_batch, 'F1-score': scores, 'execution time': execution_time,
-         'flops' : flops}
-    df = pd.DataFrame(data=d)
-    df.to_excel(globalize("/model_{}_layers/").format(layers_number) + "resume_{}_layers.xlsx".format(layers_number))
+                d = {'number of Conv2D': conv, 'number of Dense': dense, 'f': col_f, 'd': col_d,
+                     'batch size': col_batch, 'F1-score': scores, 'execution time': execution_time}
 
-    print("Dataframe is saved as xlsx")
+                df = pd.DataFrame(data=d)
+                df.to_excel(globalize("/model_{}_layers_size_{}_{}/").format(layers_number, shape, shape) +
+                            "resume_{}_layers.xlsx".format(layers_number))
+
+                print("Updated Dataframe is saved as xlsx")
+
+    print("Building Final Dataframe")
+    print("Final Dataframe is saved as xlsx")
 
 build_model()
