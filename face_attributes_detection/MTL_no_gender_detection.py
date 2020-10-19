bd9c643bc3056f7569e18029b3e133c06abd60b7
Ismail Oussaid <59666904+ismailoussaid@users.noreply.github.com>
2020-10-19 15:13:04 +0200

    Update MTL_no_gender_detection.py

--- face_attributes_detection/MTL_no_gender_detection.py
+++ face_attributes_detection/MTL_no_gender_detection.py
@@ -147,28 +147,6 @@ def f1(y_true, y_pred):  # taken from old keras source code
     f1_val = 2 * (precision * recall) / (precision + recall + K.epsilon())
     return f1_val
 
-def plot_epoch(history, metric, filename):
-    # takes history of model.fit and extract training and validation metric data
-    # save the curve in filename
-    plt.figure()
-    horizontal_axis = np.array([epoch for epoch in range(1, len(history.history[metric]) + 1)])
-    # Plot the metric curves for training and validation
-    training = np.array(history.history[metric])
-    validation = np.array(history.history['val_{}'.format(metric)])
-    plt.plot(horizontal_axis, training)
-    plt.plot(horizontal_axis, validation)
-    plt.legend(['Training {}'.format(metric), 'Validation {}'.format(metric)])
-    if 'loss' not in metric:
-        maximum = max(validation)
-        argmaximum = np.argmax(validation)
-        plt.title("max val {} :{:.4f} for epoch: {}".format(metric, maximum, horizontal_axis[argmaximum]))
-    else:
-        minimum = min(validation)
-        argminimum = np.argmin(validation)
-        plt.title("min val {} :{:.4f} for epoch: {}".format(metric, minimum, horizontal_axis[argminimum]))
-    plt.savefig(filename)
-    plt.close()
-
 def get_flops(model_h5_path):
     # computes floating points operations for a h5 model
     session = tf.compat.v1.Session()
@@ -242,13 +220,14 @@ class FaceNet:
 class CelebASequence(Sequence):
     TEST_PROPORTION = 0.1
 
-    def __init__(self, attributes_path, images_path, batch_size, shape, channel, max_items=None, n_split=5):
+    def __init__(self, attributes_path, images_path, batch_size, shape, channel, samples_per_data=6, max_items=None, n_split=5):
         self.images_path = images_path
         self.batch_size = batch_size
         self.sizes = (shape, shape, channel)
-        self.samples_per_data = 4
+        self.samples_per_data = samples_per_data
+
         if batch_size % self.samples_per_data != 0:  # VERY debatable
-            raise NotImplementedError('Batch size has to be multiple of 4')
+            raise NotImplementedError(f'Batch size has to be multiple of {self.samples_per_data}')
 
         self.attributes_tab = pd.read_csv(attributes_path)
         if max_items is not None:
@@ -297,6 +276,19 @@ class CelebASequence(Sequence):
         atts = {'mustache': [], 'eyeglasses': [], 'beard': [], 'hat': [], 'bald': []}
         j = 0
 
+        # The greater the size, the more the motion.
+        kernel_size = 30
+        # Create the vertical kernel.
+        kernel_v = np.zeros((kernel_size, kernel_size))
+        # Create a copy of the same for creating the horizontal kernel.
+        kernel_h = np.copy(kernel_v)
+        # Fill the middle row with ones.
+        kernel_v[:, int((kernel_size - 1) / 2)] = np.ones(kernel_size)
+        kernel_h[int((kernel_size - 1) / 2), :] = np.ones(kernel_size)
+        # Normalize.
+        kernel_v /= kernel_size
+        kernel_h /= kernel_size
+
         for k in range(st, sp):
             if self.mode == 0:
                 index = self.input_train[self.fold][k]
@@ -310,11 +302,15 @@ class CelebASequence(Sequence):
             img_b = cv2.blur(img, (2, 2)).reshape(self.sizes)
             img_m = cv2.flip(img, 1).reshape(self.sizes)
             img_mb = cv2.flip(img_b, 1).reshape(self.sizes)
+            img_mbv = cv2.filter2D(img, -1, kernel_v).reshape(self.sizes)
+            img_mbh = cv2.filter2D(img, -1, kernel_h).reshape(self.sizes)
 
             imgs[j, :, :, :] = img
             imgs[j + 1, :, :, :] = img_b
             imgs[j + 2, :, :, :] = img_m
             imgs[j + 3, :, :, :] = img_mb
+            imgs[j + 4, :, :, :] = img_mbv
+            imgs[j + 5, :, :, :] = img_mbh
             j += self.samples_per_data
 
             for a in self.attributes:
@@ -345,10 +341,10 @@ class CelebASequence(Sequence):
                         atts[name].append(anti_adapt(self.attributes_tab[a][index]))
 
 k_sizes = [(3,3)]
-first_convs = [4, 8]
-second_convs = [8, 16]
+first_convs = [8, 16]
+second_convs = [16, 32]
 units = [8, 16]
-batch_sizes = [64, 128, 256]
+batch_sizes = [72, 144]
 
 def main(epochs, max_items, folds, skip):
     shape, channel, compute_flops = 36, 1, True
@@ -456,7 +452,7 @@ def main(epochs, max_items, folds, skip):
 
                         print(dict_col)
                         df = pd.DataFrame(data=dict_col)
-                        df.to_excel(root_path+"tab_comparison_no_gender.xlsx")
+                        df.to_excel(root_path+"tab_comparison_no_gender_motion.xlsx")
                         print("Updated Dataframe is saved as xlsx")
 
     print("Final Dataframe is saved as xlsx")
@@ -474,7 +470,7 @@ if __name__ == '__main__':
 
     max_items = None
     epochs = 10
-    folds = 1
+    folds = 5
     skip = 0
     for o, a in opts:
         if o in ('-e', '--epochs'):
