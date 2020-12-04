6d3ed0fb1b1f548b577baffeb490dfa8af199e91
ismailoussaid <59666904+ismailoussaid@users.noreply.github.com>
2020-12-04 08:45:44 +0100

    +option of input folder, output folder

--- car_detection/object_detection_image_ai.py
+++ car_detection/object_detection_image_ai.py
@@ -1,7 +1,5 @@
 from imageai.Detection import ObjectDetection
-import numpy as np
 import os
-import cv2
 import glob
 import pandas as pd
 import time
@@ -10,21 +8,18 @@ from utils import *
 #Requirements: tensorflow==1.13.0
 items = (0,-1)
 threshold = 75
-root_path = "C:/Users/Ismail/Documents/Projects/Detect Cars/"
 shape, channel = 608, 3
-networks = ["yolo-tiny", "resnet", "yolo"]
-
-def globalize(path, root = root_path):
-    return root + path
+networks = ["yolo", "yolo-tiny", "resnet"]
 
 model_yolo = globalize("yolo_v3.h5")
 model_resnet = globalize("resnet50_coco_best_v2.1.0.h5")
 model_yolo_tiny = globalize("yolo-tiny.h5")
 image_output = globalize("Detect Cars/output_test.jpg")
-images_path = sorted(glob.glob(globalize('dataset_car_detection/*.jpg')), key=os.path.getmtime)
+images_folder = globalize('rainy_frames/*.jpg')
 
-def detect_imageai(label = "yolo", threshold=threshold, items=items):
+def detect_imageai(label = "yolo", threshold=threshold, items=items, input_path=images_folder ,detection_output_folder="results/"):
     detector = ObjectDetection()
+    images_path = sorted(glob.glob(input_path), key=os.path.getmtime)
 
     if label == "yolo":
         detector.setModelTypeAsYOLOv3()
@@ -39,7 +34,7 @@ def detect_imageai(label = "yolo", threshold=threshold, items=items):
     detector.loadModel()
     custom = detector.CustomObjects(car=True, truck=True, motorcycle=True, bus=True)
 
-    output_folder = globalize("outputs/")
+    output_folder = globalize('rainy_frames/')+detection_output_folder
     build_folder(output_folder)
 
     x1, y1, ws, hs, filenames, objects, probas = [], [], [], [], [], [], []
@@ -72,12 +67,12 @@ def detect_imageai(label = "yolo", threshold=threshold, items=items):
             h = y_max - y_min
             object = item["name"]
             proba = item['percentage_probability']
-            filename = path[len("C:/Users/Ismail/Documents/Projects/Detect Cars/dataset_car_detection//")-1:]
+            filename = path[len("C:/Users/Ismail/Documents/Projects/Detect Cars/rainy_frames//")-1:]
             multiple_append([x1, y1, ws, hs, objects, filenames, probas],
                             [x_min, y_min, w, h, object, filename, proba])
 
             tab = pd.DataFrame(data=d)
-            tab.to_csv(globalize(f"tab_{label}.csv"))
+            tab.to_csv(globalize(f"tab_rainy_{label}.csv"))
 
     return tab
 
