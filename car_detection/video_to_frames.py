1136a5a8f21c34d3689b22930ed699519b955bff
ismailoussaid <59666904+ismailoussaid@users.noreply.github.com>
2020-12-07 08:51:04 +0100

    added parameter freq to choose frequence of frames to add to folder

--- car_detection/video_to_frames.py
+++ car_detection/video_to_frames.py
@@ -2,18 +2,21 @@ import cv2
 from utils import *
 
 global_path = "C:/Users/Ismail/Documents/Projects/Detect Cars/"
+freq = 5
 
 # Opens the Video file
-cap = cv2.VideoCapture(globalize('rainy-evening-on-airline-highway-traffic-webcam.mp4'))
-folder = globalize('rainy_frames/')
+cap = cv2.VideoCapture(globalize('shanghai-traffic-rainy-afternoon.mp4'))
+folder = globalize('rainy_frames_2/')
 build_folder(folder)
 i = 0
+
 while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == False:
         break
     frame = frame
-    cv2.imwrite(folder + 'img_' + str(i) + '.jpg', frame)
+    if i%5 == 0:
+        cv2.imwrite(folder + 'img_' + str(i//freq) + '.jpg', frame)
     i += 1
 
 cap.release()
