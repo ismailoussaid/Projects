d2864fac0efad977270e08e2bb3d207ac21f6774
ismailoussaid <59666904+ismailoussaid@users.noreply.github.com>
2020-12-08 09:05:17 +0100

    added argparse

--- car_detection/video_to_frames.py
+++ car_detection/video_to_frames.py
@@ -1,23 +1,34 @@
 import cv2
 from utils import *
+from argparse import ArgumentParser
 
 global_path = "C:/Users/Ismail/Documents/Projects/Detect Cars/"
-freq = 5
 
-# Opens the Video file
-cap = cv2.VideoCapture(globalize('shanghai-traffic-rainy-afternoon.mp4'))
-folder = globalize('rainy_frames_2/')
-build_folder(folder)
-i = 0
+def build_argparser():
+    parser = ArgumentParser(add_help=False)
+    args = parser.add_argument_group("Options")
+    args.add_argument('-v', '--video', help='Path to video file', required=True, type=str)
+    args.add_argument('-o', '--output_folder', help='output folder of frames', required=True, type=str)
+    args.add_argument('-f', '--freq', default=1, type=int)
+    return parser
 
-while (cap.isOpened()):
-    ret, frame = cap.read()
-    if ret == False:
-        break
-    frame = frame
-    if i%5 == 0:
-        cv2.imwrite(folder + 'img_' + str(i//freq) + '.jpg', frame)
-    i += 1
+if __name__ == '__main__':
+    args = build_argparser().parse_args()
+    print(args.video)
+    # Opens the Video file
+    cap = cv2.VideoCapture(globalize(args.video))
+    folder = globalize(args.output_folder)
+    build_folder(folder)
+    i = 0
 
-cap.release()
-cv2.destroyAllWindows()
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        frame = frame
+        if i%args.freq == 0:
+            cv2.imwrite(folder + 'img_' + str(i//args.freq) + '.jpg', frame)
+        i += 1
+
+    cap.release()
+    cv2.destroyAllWindows()
