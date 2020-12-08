1267419f1968129fb4f080228f36a5c1a06ede47
ismailoussaid <59666904+ismailoussaid@users.noreply.github.com>
2020-12-08 09:10:41 +0100

    corrected repository writing error

--- car_detection/video_to_frames.py
+++ car_detection/video_to_frames.py
@@ -8,13 +8,13 @@ def build_argparser():
     parser = ArgumentParser(add_help=False)
     args = parser.add_argument_group("Options")
     args.add_argument('-v', '--video', help='Path to video file', required=True, type=str)
-    args.add_argument('-o', '--output_folder', help='output folder of frames', required=True, type=str)
+    args.add_argument('-o', '--output_folder', help='Name of the output folder of frames', required=True, type=str)
     args.add_argument('-f', '--freq', default=1, type=int)
     return parser
 
 if __name__ == '__main__':
     args = build_argparser().parse_args()
-    print(args.video)
+
     # Opens the Video file
     cap = cv2.VideoCapture(globalize(args.video))
     folder = globalize(args.output_folder)
@@ -27,7 +27,7 @@ if __name__ == '__main__':
             break
         frame = frame
         if i%args.freq == 0:
-            cv2.imwrite(folder + 'img_' + str(i//args.freq) + '.jpg', frame)
+            cv2.imwrite(folder + '/img_' + str(i//args.freq) + '.jpg', frame)
         i += 1
 
     cap.release()
