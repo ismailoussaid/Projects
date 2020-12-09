45e98e018799848f12e6940589e313a8eff0aedb
ismailoussaid <59666904+ismailoussaid@users.noreply.github.com>
2020-12-09 15:39:59 +0100

    split video into frames with argparser

--- car_detection/video_to_frames.py
+++ car_detection/video_to_frames.py
@@ -2,7 +2,7 @@ import cv2
 from utils import *
 from argparse import ArgumentParser
 
-global_path = "C:/Users/Ismail/Documents/Projects/Detect Cars/"
+global_path = "C:/frames/"
 
 def build_argparser():
     parser = ArgumentParser(add_help=False)
@@ -10,14 +10,22 @@ def build_argparser():
     args.add_argument('-v', '--video', help='Path to video file', required=True, type=str)
     args.add_argument('-o', '--output_folder', help='Name of the output folder of frames', required=True, type=str)
     args.add_argument('-f', '--freq', default=1, type=int)
+    args.add_argument('-b', '--blur', default=1, help='Size of average filter', required=True, type=int)
+    args.add_argument('-t', '--type', default='average', help='Type of filter', required=True, type=str)
     return parser
 
 if __name__ == '__main__':
     args = build_argparser().parse_args()
+    type, blur = args.type, args.blur
 
     # Opens the Video file
     cap = cv2.VideoCapture(globalize(args.video))
-    folder = globalize(args.output_folder)
+
+    if args.blur != 1:
+        folder = globalize(args.output_folder + f'_{type}_{blur}', root = global_path)
+    else:
+        folder = globalize(args.output_folder, root = global_path)
+
     build_folder(folder)
     i = 0
 
@@ -27,6 +35,11 @@ if __name__ == '__main__':
             break
         frame = frame
         if i%args.freq == 0:
+            size = (blur, blur)
+            if type == 'average':
+                frame = cv2.blur(frame, size)
+            elif type == 'gaussian':
+                frame = cv2.GaussianBlur(frame, size, 0)
             cv2.imwrite(folder + '/img_' + str(i//args.freq) + '.jpg', frame)
         i += 1
 
