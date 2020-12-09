7af8b064e94d5dbb9cbf6a65b461496e7a385afb
ismailoussaid <59666904+ismailoussaid@users.noreply.github.com>
2020-12-09 15:47:41 +0100

    added new argument of argparser and deleted useless lines

--- car_detection/network_scoring.py
+++ car_detection/network_scoring.py
@@ -1,51 +1,28 @@
 
 import pandas as pd
 from utils import *
+from shapely.geometry import Polygon
+from argparse import ArgumentParser
 
-items = None
-tab_base = "tab_rainy"
-tab_yolo = pd.read_csv(globalize(f"{tab_base}_yolo.csv"))
-tab_resnet = pd.read_csv(globalize(f"{tab_base}_resnet.csv"))
-tab_yolo_tiny = pd.read_csv(globalize(f"{tab_base}_yolo-tiny.csv"))
-tab_vehicle_detection_adas_0002 = pd.read_csv(globalize(f"{tab_base}_vehicle-detection-adas-0002.csv"))
-tab_vehicle_detection_adas_binary_0001 = pd.read_csv(globalize(f"{tab_base}_vehicle-detection-adas-binary-0001.csv"))
-tab_person_vehicle_bike_detection_crossroad_0078 = pd.read_csv(globalize(f"{tab_base}_person-vehicle-bike-detection-crossroad-0078.csv"))
-tab_person_vehicle_bike_detection_crossroad_1016 = pd.read_csv(globalize(f"{tab_base}_person-vehicle-bike-detection-crossroad-1016.csv"))
-tab_pedestrian_and_vehicle_detector_adas_0001 = pd.read_csv(globalize(f"{tab_base}_pedestrian-and-vehicle-detector-adas-0001.csv"))
+def build_argparser():
+    parser = ArgumentParser(add_help=False)
+    args = parser.add_argument_group("Options")
+    args.add_argument('-f', '--filenames', help='Tab of filenames', required=True, type=str)
+    args.add_argument('-t', '--tab_base', help='Name of the csv tab', required=True, type=str)
+    args.add_argument('-i', '--items', help='Number of items is maximum if None', required=True, default=None)
+    return parser
 
-tabs = [tab_yolo, tab_resnet, tab_yolo_tiny,
-        tab_vehicle_detection_adas_0002, tab_vehicle_detection_adas_binary_0001,
-        tab_person_vehicle_bike_detection_crossroad_0078, tab_person_vehicle_bike_detection_crossroad_1016,
-        tab_pedestrian_and_vehicle_detector_adas_0001]
-names = ["yolo", "resnet", "yolo_tiny",
-        "vehicle_detection_adas_0002", "vehicle_detection_adas_binary_0001",
-        "person_vehicle_bike_detection_crossroad_0078", "person_vehicle_bike_detection_crossroad_1016",
-        "pedestrian_and_vehicle_detector_adas_0001"]
-
-def iou(boxA, boxB):
-
-    xA = max(boxA['x1'], boxB['x1'])
-    yA = max(boxA['y1'], boxB['y1'])
-    xB = min(boxA['x1']+boxA['w'], boxB['x1']+boxA['w'])
-    yB = min(boxA['y1']+boxA['h'], boxB['y1']+boxB['h'])
-
-    # compute the area of intersection rectangle
-    interArea = abs(max((xB - xA+1, 0)) * max((yB - yA+1, 0)))
-
-    if interArea == 0:
-        return 0
-
-    # compute the area of both the prediction and ground-truth
-    # rectangles
-    boxAArea = abs((boxA['w']+1) * (boxA['h']+1))
-    boxBArea = abs((boxB['h']+1) * (boxB['h']+1))
-
-    # compute the intersection over union by taking the intersection
-    # area and dividing it by the sum of prediction + ground-truth
-    # areas - the intersection area
-    iou = interArea / float(boxAArea + boxBArea - interArea)
-
-    # return the intersection over union value
+def iou(bbox_1, bbox_2):
+    def transform(bbox):
+        x1 = bbox['x1']
+        y1 = bbox['y1']
+        h = bbox['h']
+        w = bbox['w']
+        box = [[x1,y1], [x1+w,y1], [x1+w, y1+h], [x1, y1+h]]
+        return box
+    poly_1 = Polygon(transform(bbox_1))
+    poly_2 = Polygon(transform(bbox_2))
+    iou = poly_1.intersection(poly_2).area / poly_1.union(poly_2).area
     return iou
 
 def cluster_subtab(sub_tab, threshold):
@@ -61,13 +38,13 @@ def cluster_subtab(sub_tab, threshold):
     objects = []
 
     for k in range(n-1):
-
         data = df[k]
         list_iou = []
 
         for i in range(k+1,n):
             obj = df[i]
-            list_iou.append(iou(data, obj))
+            area = iou(data, obj)
+            list_iou.append(area)
 
         boolean = [x < threshold for x in list_iou]
         if all(boolean):
@@ -77,8 +54,8 @@ def cluster_subtab(sub_tab, threshold):
 
     clusters = []
     for i in range(len(objects)):
-        cluster = []
         object = objects[i]
+        cluster = [object]
         for k in range(len(df)):
             element = df[k]
             area = iou(object, element)
@@ -99,6 +76,7 @@ def avg_cluster(clusters):
             avgDict = {}
             p = len(cluster)
             for key, v in cluster[0].items():
+
                 if type(v) == str:
                     avgDict[key] = v
                 else:
@@ -114,7 +92,6 @@ def avg_cluster(clusters):
                     avgDict[key] = round(v/p)
 
             detections.append(avgDict)
-
     return detections
 
 def binary(liste):
@@ -131,11 +108,9 @@ def compare(sub_tab_1, sub_tab_2, methode=binary):
     scores = []
 
     if n1 == 0:
-        #print("no detection in strong learner")
         return 0
 
     elif n2 == 0:
-        #print("no detection in weak learner")
         return 0
 
     else:
@@ -153,7 +128,6 @@ def confusion_compare(sub_tab_1, sub_tab_2, comparison_threshold):
     #tp is the amount of vehicles detected by network1 and by network 2
     #tn is the amount of elements not identified as vehicles by network1 but detected by network 2
     #fn is the amount of vehicles detected by network1 and not by network 2  100
-    
     n1, n2 = len(sub_tab_1), len(sub_tab_2)
     tp, tn, fn = 0, 0, 0
 
@@ -174,7 +148,7 @@ def confusion_compare(sub_tab_1, sub_tab_2, comparison_threshold):
 
         for k in range(len(sub_tab_1)):
             for j in range(len(sub_tab_2)):
-                if k in deleted_s1 or j in deleted_s1:
+                if k in deleted_s1 or j in deleted_s2:
                     continue
                 area = iou(sub_tab_1[k], sub_tab_2[j])
                 if area >= comparison_threshold:
@@ -190,23 +164,23 @@ def confusion_compare(sub_tab_1, sub_tab_2, comparison_threshold):
 
         return tp, tn, fn
 
-def score(tab1, tab2, averaging_threshold, comparison_threshold=0.2, scr='scoring', methode = binary, items=items):
+def score(tab1, tab2, averaging_threshold, filenames=filenames, comparison_threshold=0.2, scr='scoring', methode = binary, items=items):
     if items == None or items == (0, -1):
         a,b = 0,-1
     else:
         a,b = items
 
-    filenames = tab1['file']
     #all distinct filename
-    filenames = filenames.drop_duplicates().iloc[a:b]
-
+    filenames = filenames.iloc[a:b]['file']
     scores = []
     tp_scores, tn_scores, fp_scores = [], [], []
 
     for filename in filenames:
-        subtab_1 = avg_cluster(cluster_subtab(tab1[tab1.file == filename],
+        tab_1 = tab1[tab1.file == filename]
+        tab_2 = tab2[tab2.file == filename]
+        subtab_1 = avg_cluster(cluster_subtab(tab_1,
                                               threshold=averaging_threshold))
-        subtab_2 = avg_cluster(cluster_subtab(tab2[tab2.file == filename],
+        subtab_2 = avg_cluster(cluster_subtab(tab_2,
                                               threshold=averaging_threshold))
         if scr == 'scoring':
             score = compare(subtab_1, subtab_2, methode)
@@ -223,15 +197,40 @@ def score(tab1, tab2, averaging_threshold, comparison_threshold=0.2, scr='scorin
         return sum(tp_scores), sum(tn_scores), sum(fp_scores)
 
 if __name__ == '__main__':
+
+    args = build_argparser().parse_args()
+
+    filenames = pd.read_csv(globalize(f"filenames_{args.filenames}.csv"))
+    tab_yolo = pd.read_csv(globalize(f"{args.tab_base}_yolo.csv"))
+    tab_resnet = pd.read_csv(globalize(f"{args.tab_base}_resnet.csv"))
+    tab_yolo_tiny = pd.read_csv(globalize(f"{args.tab_base}_yolo-tiny.csv"))
+    tab_vehicle_detection_adas_0002 = pd.read_csv(globalize(f"{args.tab_base}_vehicle-detection-adas-0002.csv"))
+    tab_vehicle_detection_adas_binary_0001 = pd.read_csv(
+        globalize(f"{args.tab_base}_vehicle-detection-adas-binary-0001.csv"))
+    tab_person_vehicle_bike_detection_crossroad_0078 = pd.read_csv(
+        globalize(f"{args.tab_base}_person-vehicle-bike-detection-crossroad-0078.csv"))
+    tab_person_vehicle_bike_detection_crossroad_1016 = pd.read_csv(
+        globalize(f"{args.tab_base}_person-vehicle-bike-detection-crossroad-1016.csv"))
+    tab_pedestrian_and_vehicle_detector_adas_0001 = pd.read_csv(
+        globalize(f"{args.tab_base}_pedestrian-and-vehicle-detector-adas-0001.csv"))
+
+    tabs = [tab_yolo, tab_resnet, tab_yolo_tiny,
+            tab_vehicle_detection_adas_0002, tab_vehicle_detection_adas_binary_0001,
+            tab_person_vehicle_bike_detection_crossroad_0078, tab_person_vehicle_bike_detection_crossroad_1016,
+            tab_pedestrian_and_vehicle_detector_adas_0001]
+    names = ["yolo", "resnet", "yolo_tiny",
+             "vehicle_detection_adas_0002", "vehicle_detection_adas_binary_0001",
+             "person_vehicle_bike_detection_crossroad_0078", "person_vehicle_bike_detection_crossroad_1016",
+             "pedestrian_and_vehicle_detector_adas_0001"]
+
     s = 0.1
     names1, names2, tps, fps, fns = [], [], [], [], []
     data = {'network 1': names1, 'network 2': names2, 'tp': tps, 'fp': fps, 'fn': fns}
     n = len(names)
-    for i in range(n):
-        for j in range(n):
+    for i in range(n-1):
+        for j in range(i+1,n):
             name1, name2, tab1, tab2 = names[i], names[j], tabs[i], tabs[j]
-            if name1 != name2:
-                tp, fp, fn = score(tab1, tab2, s, scr='confusion')
-                multiple_append([names1, names2, tps, fps, fns], [name1, name2, tp, fp, fn])
-                tab = pd.DataFrame(data=data)
-                tab.to_csv(globalize(f'scores_comparative_{tab_base[4:]}.csv'))
+            tp, fp, fn = score(tab1, tab2, s, scr='confusion')
+            multiple_append([names1, names2, tps, fps, fns], [name1, name2, tp, fp, fn])
+            tab = pd.DataFrame(data=data)
+            tab.to_csv(globalize(f'scores_comparative_{args.tab_base[4:]}.csv'))
